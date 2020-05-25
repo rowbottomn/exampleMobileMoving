f62e109ef3f69955274326ad55d696bfffa908f2
Nathan Rowbottom <rowbottom_at_hdsb.ca>
2020-05-25 16:51:49 +0000

    Attempts to modify direction to move

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ var pos = new THREE.Vector3();
 var vel = new THREE.Vector3();
 var dir = new THREE.Vector3();
 var rot = new THREE.Vector3();
-const speed = 0.3;
+const speed = -0.005;
 const rest = new THREE.Vector3(0, 0, 0);
 
 AFRAME.registerComponent('rotation-reader', {
-  init: function{
-    this.tick = AFRAME.utils.throttleTick(this.tick, 300, this);
-  }
+  init: function(){
+    this.tick = AFRAME.utils.throttleTick(this.tick, 100, this);
+  },
   tick: function (t, dt) {
   
     // `this.el` is the element.
@@ -20,19 +20,24 @@ AFRAME.registerComponent('rotation-reader', {
 
     rot = el.getAttribute('rotation');
     //pos = el.getAttribute('position');
-    //dir = el.getAttribute('direction');
+    dir = el.getAttribute('direction');
+    //dir = dir.unproject(this.el.camera);
     if (rot.x < -Math.PI/4){
       //vel = speed;      
-      el.object3DtranslateZ(speed);
+      //el.object3D.translateZ(speed*dt);
+      positionTmp.z += dir.z
+      //el.setAttribute('rotation', rotationTmp);
       //pos.add(vel);
     }
     else {
       //vel = rest;
     }
-    console.log(this.el.object3D.rotation);
+
+    el.setAttribute('position', positionTmp);
+    //console.log(this.el.object3D.rotation);
 
     // `position` is a three.js Vector3.
-    console.log(this.el.object3D.position);
+   // console.log(this.el.object3D.position);
   }
 });
 /*
